36f377b4c8e6b20bddfc600840719e7af8af39d8
iceice666@archlinux <iceice666@outlook.com>
2022-11-17 16:24:07 +0800

    init

---
+++ .config/.startup.sh
@@ -0,0 +1,15 @@
+
+# startup
+
+xsetroot -cursor_name left_ptr
+
+feh --randomize --bg-fill ~/wallpaper.jpg
+
+# ~/.config/polybar/luanch.sh
+
+picom  &
+fcitx5 &
+blueman-applet &
+polybar &
+greenclip daemon &
+
